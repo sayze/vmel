fc30dcbd0d99b06010d71dd3ab94a1f170787880
sayed <sayed.sadeed@gmail.com>
2018-03-15 08:33:39 +1000

    created utils module

--- docs/html/search/functions_4.js
+++ docs/html/search/functions_4.js
@@ -1,7 +1,11 @@
 var searchData=
 [
   ['tokenmgr_5fadd',['TokenMgr_add',['../tokens_8h.html#a281ef13a99766bd9096e2c403f2c3f60',1,'tokens.h']]],
+  ['tokenmgr_5ffirst_5ftoken',['TokenMgr_first_token',['../tokens_8h.html#aca40d3c1454484d6fba15d498810e779',1,'tokens.h']]],
   ['tokenmgr_5ffree',['TokenMgr_free',['../tokens_8h.html#a7402a62420f576f7f1ce38d0da9bea41',1,'tokens.h']]],
+  ['tokenmgr_5flast_5ftoken',['TokenMgr_last_token',['../tokens_8h.html#a09eddb1fdcf73f26cf71f589b0b831c5',1,'tokens.h']]],
   ['tokenmgr_5fnew',['TokenMgr_new',['../tokens_8h.html#a8583e6f3a8e9ab1403387fb110e18302',1,'tokens.h']]],
+  ['tokenmgr_5fnext_5ftoken',['TokenMgr_next_token',['../tokens_8h.html#abba7a2527fb5684453e62db37cc232cc',1,'tokens.h']]],
+  ['tokenmgr_5fprev_5ftoken',['TokenMgr_prev_token',['../tokens_8h.html#aab2ffb25ab0ad755354661c3d07836a3',1,'tokens.h']]],
   ['tokenmgr_5fprint_5ftokens',['TokenMgr_print_tokens',['../tokens_8h.html#a0bb266bca06cd42cfcfbf7da6de5b874',1,'tokens.h']]]
 ];
---
+++ src/utils.h
@@ -0,0 +1,10 @@
+/**
+ * @file utils.h
+ * @author Sayed Sadeed
+ * @brief File containing collection of helper functions.
+ */
+
+#ifndef UTILS_H
+#define UTILS_H
+
+#endif
